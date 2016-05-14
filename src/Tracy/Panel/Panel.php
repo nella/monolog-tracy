a44b39d02a298e29061110e130bf2078404e6830
Patrik Votocek <patrik@votocek.cz>
2016-05-14 19:41:37 +0200

    Move Panel from bundle repo

---
+++ src/Tracy/Panel/Panel.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * This file is part of the Nella Project (https://monolog-tracy.nella.io).
+ *
+ * Copyright (c) Patrik Votoček (https://patrik.votocek.cz)
+ *
+ * For the full copyright and license information,
+ * please view the file LICENSE.md that was distributed with this source code.
+ */
+
+namespace Nella\MonologTracy\Tracy\Panel;
+
+abstract class Panel
+{
+
+	/**
+	 * @param \Exception|\Throwable $exception
+	 * @return bool
+	 */
+	abstract public function isSupported($exception);
+
+	/**
+	 * @param \Exception|\Throwable $exception
+	 * @return string
+	 */
+	abstract public function getTab($exception);
+
+	/**
+	 * @param \Exception|\Throwable $exception
+	 * @return string
+	 */
+	abstract public function getPanel($exception);
+
+	/**
+	 * @param \Exception|\Throwable $exception
+	 * @return string[]
+	 */
+	final public function __invoke($exception)
+	{
+		if (!$this->isSupported($exception)) {
+			return [];
+		}
+
+		return [
+			'tab' => $this->getTab($exception),
+			'panel' => $this->getPanel($exception),
+		];
+	}
+
+}
